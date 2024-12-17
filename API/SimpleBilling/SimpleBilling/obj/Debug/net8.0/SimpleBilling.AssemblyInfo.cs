68c99a2e4cc5a09d15365f422242087102b52464
Anisul Haque <anisjob1@gmail.com>
2024-12-17 20:47:39 +0100

    Change port for access to backend v1

--- API/SimpleBilling/SimpleBilling/obj/Debug/net8.0/SimpleBilling.AssemblyInfo.cs
+++ API/SimpleBilling/SimpleBilling/obj/Debug/net8.0/SimpleBilling.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleBilling")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+267bbd046076b86b1036af6fd8cae9b2b7e71263")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42ae65419fcce97370f404071d53286dc384da69")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleBilling")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleBilling")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
