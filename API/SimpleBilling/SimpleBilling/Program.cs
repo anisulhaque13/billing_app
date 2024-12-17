280bdadf652206f4f4a0f175def3812e8993d820
Anisul Haque <anisjob1@gmail.com>
2024-12-17 21:49:39 +0100

    path modified for backend v2

--- API/SimpleBilling/SimpleBilling/Program.cs
+++ API/SimpleBilling/SimpleBilling/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: MyAllowSpecificOrigins, policy =>
     {
-        policy.WithOrigins("http://34.58.234.179:8081") // Add your client app's URL
+        policy.WithOrigins("https://23.251.152.29") // Add your client app's URL
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
--- API/SimpleBilling/SimpleBilling/obj/Debug/net8.0/SimpleBilling.AssemblyInfo.cs
+++ API/SimpleBilling/SimpleBilling/obj/Debug/net8.0/SimpleBilling.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleBilling")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fee36fc9d49dba614ab6e4a576802ec19c77017")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e14897653bfa5e6d2dc66771fbd7e02cad3ee2a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleBilling")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleBilling")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
