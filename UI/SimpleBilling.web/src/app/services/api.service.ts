a88798b73ff2b6e9f013d1ec4d73a94f1797193c
Anisul Haque <anisjob1@gmail.com>
2024-12-20 06:54:55 +0100

    ASWS configure newly v5

--- UI/SimpleBilling.web/src/app/services/api.service.ts
+++ UI/SimpleBilling.web/src/app/services/api.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'  // Makes the service available application-wide
 })
 export class ApiService {
-  baseUrl = 'https://localhost:5001';  // Global URL
+  baseUrl = 'https://localhost:8080';  // Global URL
 
   getUrl(endpoint: string): string {
     return `${this.baseUrl}${endpoint}`;
