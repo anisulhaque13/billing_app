0b75ff856613f940a9f987ab2ab4d54fd6ba77d7
Anisul Haque <anisjob1@gmail.com>
2024-12-20 01:12:26 +0100

    docker https remove temp

--- API/SimpleBilling/SimpleBilling/Program.cs
+++ API/SimpleBilling/SimpleBilling/Program.cs
@@ -19,10 +19,10 @@ builder.Services.AddDbContext<BillingDBContext>(options =>
 builder.WebHost.ConfigureKestrel(options =>
 {
     options.ListenAnyIP(8080); // HTTP
-    options.ListenAnyIP(8081, listenOptions =>
-    {
-        listenOptions.UseHttps("/https/https-dev-cert.pfx", "121");
-    });
+    //options.ListenAnyIP(8081, listenOptions =>
+    //{
+    //    listenOptions.UseHttps("/https/https-dev-cert.pfx", "121");
+    //});
 });
 
 var app = builder.Build();
