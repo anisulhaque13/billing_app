dc295a07851664d45bd53cc34b0a40339fafd1f7
Anisul Haque <anisjob1@gmail.com>
2024-12-21 01:28:54 +0100

    aws service.cluster path added for api access

--- UI/SimpleBilling.web/src/app/services/api.service.ts
+++ UI/SimpleBilling.web/src/app/services/api.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'  // Makes the service available application-wide
 })
 export class ApiService {
-  baseUrl = 'http://localhost:8080';  // Global URL
+  baseUrl = 'http://simplebilling-service.simplebilling-cluster:8080';  // Global URL
 
   getUrl(endpoint: string): string {
     return `${this.baseUrl}${endpoint}`;
