98c745a3a0760b0752178370a1c199cda68b2f58
Anisul Haque <anisjob1@gmail.com>
2024-12-22 20:12:13 +0100

    api connection test angular to simplebilling app v3

--- UI/SimpleBilling.web/src/app/services/api.service.ts
+++ UI/SimpleBilling.web/src/app/services/api.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'  // Makes the service available application-wide
 })
 export class ApiService {
-  baseUrl = 'http://simplebilling:8080';  // Global URL
+  baseUrl = 'http://127.0.0.1:8080';  // Global URL
 
   getUrl(endpoint: string): string {
     return `${this.baseUrl}${endpoint}`;
