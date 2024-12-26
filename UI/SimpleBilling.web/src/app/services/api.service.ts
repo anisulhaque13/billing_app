7d35c980ab97841c2d947f2f0ae21098963a2760
Anisul Haque <anisjob1@gmail.com>
2024-12-26 02:03:57 +0100

    backend url test

--- UI/SimpleBilling.web/src/app/services/api.service.ts
+++ UI/SimpleBilling.web/src/app/services/api.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'  // Makes the service available application-wide
 })
 export class ApiService {
-  baseUrl = 'https://simplebilling.azurewebsites.net:8081';  // Global URL
+  baseUrl = '/api';  // Global URL
 
   getUrl(endpoint: string): string {
     return `${this.baseUrl}${endpoint}`;
