39607b4acf26e379da4545dc52a98785546ad290
Anisul Haque <anisjob1@gmail.com>
2024-12-28 01:16:38 +0100

    google ip set all apps v2

--- UI/SimpleBilling.web/src/app/services/api.service.ts
+++ UI/SimpleBilling.web/src/app/services/api.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'  // Makes the service available application-wide
 })
 export class ApiService {
-  baseUrl = 'https://34.58.234.179:8081';  // Global URL
+  baseUrl = 'http://34.58.234.179:8081';  // Global URL
 
   getUrl(endpoint: string): string {
     return `${this.baseUrl}${endpoint}`;
