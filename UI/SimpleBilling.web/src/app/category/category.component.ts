fdf7602a218ee6a8624a34c7ce1b49b49bc54936
Anisul Haque <anisjob1@gmail.com>
2024-12-29 04:40:19 +0100

    Item, category and Order view fix

--- UI/SimpleBilling.web/src/app/category/category.component.ts
+++ UI/SimpleBilling.web/src/app/category/category.component.ts
@@ -82,7 +82,7 @@ export class CategoryComponent implements OnInit {
   private getCategories(): Observable<Category[]> {
     return this.http.get<any>(this.apiService.getUrl('/api/Category'))
       .pipe(
-       // map((response) => response.$values || []), // Extract $values array
+        map((response) => response.$values || []), // Extract $values array
         catchError((err) => {
           console.error('Error fetching categories:', err);
           alert('Failed to fetch categories.');
