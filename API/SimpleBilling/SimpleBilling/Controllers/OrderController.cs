28bc0a0e1904b1d5ac59de1dabb549080ec1024c
Anisul Haque <anisjob1@gmail.com>
2024-12-29 04:06:53 +0100

    data-load on init in Order view v6

--- API/SimpleBilling/SimpleBilling/Controllers/OrderController.cs
+++ API/SimpleBilling/SimpleBilling/Controllers/OrderController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using SimpleBilling.Models.Domain;
 
 namespace SimpleBilling.Controllers
 {
+    [EnableCors]
     [Route("api/[controller]")]
     [ApiController]
     public class OrderController : ControllerBase
